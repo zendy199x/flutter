f29c9b5b500b45efbf70b7b841060934b6d9f260
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-22 02:06:41 +0700

    snackbar

---
+++ snackbar/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+//Define "root widget"
+void main() => runApp(new MyApp());//one-line function
+
+class MyApp extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  _showSnackBar() {
+    print("Show Snackbar here !");
+    final snackBar = new SnackBar(
+        content: new Text("This is a SnackBar"),
+        duration: new Duration(seconds: 3),
+        backgroundColor: Colors.green,
+        action: new SnackBarAction(label: 'OK', onPressed: (){
+          print('press Ok on SnackBar');
+        }),
+    );
+    //How to display Snackbar ?
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
+  @override
+  Widget build(BuildContext context) {
+    //build function returns a "Widget"
+    return new MaterialApp(
+      title: "SnackBar example",
+      home: new Scaffold(
+        key: _scaffoldKey,
+        appBar: new AppBar(
+          title: new Text("An example of SnackBar"),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.info),
+                onPressed: _showSnackBar)
+          ],
+        ),
+      )
+    );
+  }
+}
