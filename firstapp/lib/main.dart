833fe795ae50c8911eba4fc259cedfbbb0f16ebf
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-21 14:13:19 +0700

    navigator & route

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -2,22 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 //Define "root widget"
-void main() => runApp(new MyApp()); //one-line function
-
+void main() => runApp(new MyApp());//one-line function
 //StatefulWidget
 class RandomEnglishWords extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return new RandomEnglishWordsState(); //return a state's object. Where is the state's class ?
+    return new RandomEnglishWordsState();//return a state's object. Where is the state's class ?
   }
 }
-
 //State
-class RandomEnglishWordsState extends State<RandomEnglishWords> {
-  final _words =
-      <WordPair>[]; //Words displayed in ListView, 1 row contains 1 word
-  final _checkedWords = new Set<WordPair>(); //set contains "no duplicate items"
+class RandomEnglishWordsState extends State<RandomEnglishWords>{
+  final _words = <WordPair>[];//Words displayed in ListView, 1 row contains 1 word
+  final _checkedWords = new Set<WordPair>();//set contains "no duplicate items"
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -25,6 +22,10 @@ class RandomEnglishWordsState extends State<RandomEnglishWords> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("List of English words"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list),
+              onPressed: _pushToSavedWordsScreen)
+        ],
       ),
       body: new ListView.builder(itemBuilder: (context, index) {
         //This is an anonymous function
@@ -33,11 +34,33 @@ class RandomEnglishWordsState extends State<RandomEnglishWords> {
         if (index >= _words.length) {
           _words.addAll(generateWordPairs().take(10));
         }
-        return _buildRow(_words[index], index); //Where is _buildRow ?
+        return _buildRow(_words[index], index);//Where is _buildRow ?
       }),
     );
   }
-
+  _pushToSavedWordsScreen() {
+//    print("You pressed to the right Icon");
+    //To navigate, you must have a "route"
+    final pageRoute = new MaterialPageRoute(builder: (context) {
+      //map function = Convert this list to another list(maybe different object's type)
+      //_checkedWords(list of WordPair) => map => 
+      // converted to a lazy list(Iterable) of ListTile
+      final listTiles = _checkedWords.map( (wordPair) {
+        return new ListTile(
+          title: new Text(wordPair.asPascalCase,
+            style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+        );
+      });
+      //Now return a widget, we choose "Scaffold"
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Checked words"),
+        ),
+        body: new ListView(children: listTiles.toList(),),//Lazy list(Iterable) => List
+      );
+    });
+    Navigator.of(context).push(pageRoute);
+  }
   Widget _buildRow(WordPair wordPair, int index) {
     //This widget is for each row
     final textColor = index % 2 == 0 ? Colors.red : Colors.blue;
@@ -45,8 +68,8 @@ class RandomEnglishWordsState extends State<RandomEnglishWords> {
     return new ListTile(
       //leading = left, trailing = right. Is is correct ? Not yet
       leading: new Icon(
-        isChecked ? Icons.check_box : Icons.check_box_outline_blank,
-        color: textColor,
+          isChecked ? Icons.check_box : Icons.check_box_outline_blank,
+          color: textColor,
       ),
       title: new Text(
         wordPair.asPascalCase,
@@ -56,9 +79,9 @@ class RandomEnglishWordsState extends State<RandomEnglishWords> {
         setState(() {
           //This is an anonymous function
           if (isChecked) {
-            _checkedWords.remove(wordPair); //Remove item in a Set
+            _checkedWords.remove(wordPair);//Remove item in a Set
           } else {
-            _checkedWords.add(wordPair); //Add item to a Set
+            _checkedWords.add(wordPair);//Add item to a Set
           }
         });
       },
@@ -73,7 +96,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //build function returns a "Widget"
     return new MaterialApp(
-        title: "This is my first Flutter App",
-        home: new RandomEnglishWords()); //Widget with "Material design"
+      title: "This is my first Flutter App",
+      home: new RandomEnglishWords()
+    );//Widget with "Material design"
   }
 }
