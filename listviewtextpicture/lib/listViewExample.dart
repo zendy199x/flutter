d507b725dfebb34f3b30d12cbd0f4c1d5b192b48
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-23 04:27:28 +0700

    onTap GestureDetector

--- listviewtextpicture/lib/listViewExample.dart
+++ listviewtextpicture/lib/listViewExample.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'data.dart';
+import 'flower.dart';
 class ListViewExample extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -9,15 +10,23 @@ class ListViewExample extends StatefulWidget {
   }
 }
 class ListViewExampleState extends State<ListViewExample> {
-  List<Container> _buildListItemsFromFlowers(){
+  var selectedFlower = new Flower();
+
+  List<GestureDetector> _buildListItemsFromFlowers(){
     int index = 0;
     return flowers.map((flower){
+      var boxDecoration=index % 2 == 0?
+      new BoxDecoration(color: const Color(0xFFb0e0e6)):
+      new BoxDecoration(
+          color: const Color(0xFF7ec0ee)
+      );
+      if (selectedFlower == flower) {
+        boxDecoration = new BoxDecoration(
+            color: Colors.deepOrangeAccent
+        );
+      }
       var container = Container(
-        decoration: index % 2 == 0?
-        new BoxDecoration(color: const Color(0xFFb0e0e6)):
-        new BoxDecoration(
-            color: const Color(0xFF7ec0ee)
-        ),
+        decoration: boxDecoration,
         child: new Row(
           children: <Widget>[
             new Container(
@@ -61,7 +70,16 @@ class ListViewExampleState extends State<ListViewExample> {
         ),
       );
       index = index + 1;
-      return container;
+      final gestureDetector = GestureDetector(
+        child: container,
+        onTap:(){
+          print("You tapped to ${flower.flowerName}");
+          setState(() {
+            selectedFlower = flower;
+          });
+        }
+      );
+      return gestureDetector;
     }).toList();
   }
   @override
