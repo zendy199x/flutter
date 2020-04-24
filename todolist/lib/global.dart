32a4fdf68105ece9467d97c35fc3391802b3a57c
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-25 02:12:07 +0700

    setup todo  server project

---
+++ todolist/lib/global.dart
@@ -0,0 +1,5 @@
+// const SERVER_NAME = "localhost";
+const SERVER_NAME = "192.168.1.52"; //your server's ip address
+const URL_TODOS = 'http://$SERVER_NAME:3000/todos';
+const URL_TASKS = 'http://$SERVER_NAME:3000/tasks';
+const URL_TASKS_BY_TODOID = 'http://$SERVER_NAME:3000/tasks/todoid/';
---
+++ todolist/lib/models/todo.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:todolist/global.dart';
+
+class Todo {
+  int id;
+  String name;
+  String dueDate;
+  String description;
+  //Constructor
+  Todo({
+    this.id,
+    this.name,
+    this.dueDate,
+    this.description
+  });
+  //This is a static method that
+  factory Todo.fromJson(Map<String, dynamic> json) { //tạo ra object từ object Todos dạng key, value
+    return Todo(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      dueDate: json['duedate'] as String,
+      description: json['description'] as String
+    );
+  }
+}
+
+//Fetch data from Restful API
+Future<List<Todo>> fetchTodos(http.Client client) async {
+  //How to make these URLs in a .dart file ?
+  final response = await client.get(URL_TODOS);
+  if (response.statusCode == 200) {
+    Map<String, dynamic> mapResponse = json.decode(response.body); //jsonDecode
+    if (mapResponse["result"] == "ok") {
+      final todos = mapResponse["data"].cast<Map<String, dynamic>>(); //được các mảng gồm các object key, value dạng map
+      final listOfTodos = await todos.map<Todo>((json) {
+        return Todo.fromJson(json);
+      }).toList();
+      return listOfTodos;
+    } else {
+      return [];
+    }
+  } else {
+    throw Exception("Failed to load Todo from the Internet");
+  }
+}
