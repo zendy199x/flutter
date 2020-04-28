3ebe9c40405e6bf583bbf4d0063363c95809bd14
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-04-28 17:25:37 +0700

    setup models

---
+++ realworld/lib/models/user.dart
@@ -0,0 +1,23 @@
+//https://github.com/gothinkster/realworld/tree/master/api
+
+class User {
+  final String email;
+  final String token;
+  final String username;
+  final String bio;
+  final String image;
+
+  User({this.email, this.token, this.username, this.bio, this.image});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      email: json['email'] as String,
+      token: json['token'] as String,
+      username: json['token'] as String,
+      bio: json['bio'] as String,
+      image: (json['image']?.isEmplty ?? true)
+          ? "https://static.productionready.io/images/smiley-cyrus.jpg"
+          : json['image'] as String,
+    );
+  }
+}
